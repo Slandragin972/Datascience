90c8a73d0d35c956cb3a3ef9e01d884b6d9c0e35
Vincent RUDEAU <vrudeau@gmail.com>
2024-10-29 20:19:26 +0100

    feat: story.py with regexp

--- story.py
+++ story.py
@@ -1,7 +1,30 @@
+import re
+import chardet
+
+file_name = 'story.txt'
+
+# Detect encoding
+with open(file_name, 'rb') as file:
+    raw_data = file.read()
+    encoding = chardet.detect(raw_data)['encoding']
+    print(f"encoding = {encoding}")
+
+# with open(file_name, 'r', encoding=encoding) as file:
+#     content = file.read()
+    
 # Ouvrir le fichier en mode lecture
-with open('story.txt', 'r', encoding='utf-8') as file:
+with open('story.txt', 'r', encoding=encoding) as file:
     # Lire le contenu du fichier
     content = file.read()
+    
+# Remplacer les caractères accentués par leur équivalent sans accent
+
+content = re.sub(r'[àâ]', 'a', content)
+content = re.sub(r'[éèêë]', 'e', content)
+content = re.sub(r'[îïì]', 'i', content)
+content = re.sub(r'[ôöò]', 'o', content)
+content = re.sub(r'[ùû]', 'u', content)
+content = re.sub(r'[ç]', 'c', content)
 
 # Afficher le contenu du fichier
 print(content)
